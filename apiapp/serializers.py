78747fbe4d7c009519da8725786390eb3bb58230
magriker <magken7@gmail.com>
2025-01-17 21:48:20 +0900

    finish

--- apiapp/serializers.py
+++ apiapp/serializers.py
@@ -5,11 +5,12 @@ from apiapp.models import Article, User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-
+        fields='__all__'
 
 class Articleserializer(serializers.ModelSerializer):
     class Meta:
         model = Article
+        fields='__all__'
 
 
 
